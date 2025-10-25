a5901dde0bc97dc7a214407fce20d8fd698b73d1
Fredrick Omoyeni <fredrickyeni@example.com>
2025-10-25 08:36:03 +0100

    Update Navbar: changed brand name to Axisure

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                         href="/"
                         className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
                     >
-                        Fred
+                        Axisure
                     </a>
                     <button
                         onClick={() => setIsOpen(!isOpen)}
